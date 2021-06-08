e75687243a1feec89a6d2d8896290ba5aac128ec
Cefixime <1627889449@qq.com>
2021-06-08 23:35:18 +0800

    add index page of retailer and some icon

---
+++ controllers/Retailer.cc
@@ -0,0 +1,6 @@
+#include "Retailer.h"
+//add definition of your processing function here
+void Retailer::retailerIndex(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback,
+                             std::string identity) {
+
+}
